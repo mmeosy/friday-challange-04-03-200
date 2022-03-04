ceec95d7878301de42f6fdd3d214af2cca266905
mmeosy1 <mehmetfidan94@hotmail.de>
2022-03-04 21:39:05 +0100

    test.py auskommentiert

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-import requests
-import json
-from json import load, dumps
+
+# import requests
+# import json
+# from json import load, dumps
 
 
 
@@ -9,22 +10,22 @@ from json import load, dumps
 
 
 
-url = requests.get("https://api.covid19api.com/live/country/barbados/status/confirmed")
-response = url.json()
-y = response[-1]
-#print(y['Confirmed'])
+# url = requests.get("https://api.covid19api.com/live/country/barbados/status/confirmed")
+# response = url.json()
+# y = response[-1]
+# #print(y['Confirmed'])
 
-cases = response[-1]['Confirmed'] - response[-8]['Confirmed']
-active = response[-1]['Active'] - response [-8]['Active']
-print(cases)
-print(active)
-
-
+# cases = response[-1]['Confirmed'] - response[-8]['Confirmed']
+# active = response[-1]['Active'] - response [-8]['Active']
+# print(cases)
+# print(active)
 
 
 
 
-def writejsonfile(data):
-    with open('coronainfo.json', 'a') as file:
-        file.write(data)
+
+
+# def writejsonfile(data):
+#     with open('coronainfo.json', 'a') as file:
+#         file.write(data)
         
